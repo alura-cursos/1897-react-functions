2cb253ab3da9d4c364419835e64ee8ea5f4e59dc
Ricardo <dones.bugan@gmail.com>
2020-07-14 10:29:58 -0300

    video 2.1

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 function FormularioCadastro() {
   return (
     <form>
-      <label>Nome</label>
-      <input type="text" />
-
-      <label>Sobrenome</label>
-      <input type="text" />
-
-      <label>CPF</label>
-      <input type="text" />
+      <TextField id="nome" label="Nome" variant="outlined" fullWidth/>
+      <TextField id="sobrenome" label="Sobrenome"  variant="outlined" fullWidth/>
+      <TextField id="CPF" label="CPF"  variant="outlined" fullWidth/>
 
       <label>Promoções</label>
       <input type="checkbox" />
