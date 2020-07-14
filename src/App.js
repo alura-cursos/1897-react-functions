7bddd207fd6c6ee9d729b5fb2fa99a2690a3cdae
Ricardo <dones.bugan@gmail.com>
2020-07-14 11:02:37 -0300

    video 2.2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component, Fragment } from "react";
 import "./App.css";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 
+import {Container} from "@material-ui/core"
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <Container component="article" maxWidth="sm">
         <h1>Formulário de cadastro</h1>
         <FormularioCadastro />
-      </Fragment>
+      </Container>
     );
   }
 }
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -5,9 +5,9 @@ import TextField from "@material-ui/core/TextField";
 function FormularioCadastro() {
   return (
     <form>
-      <TextField id="nome" label="Nome" variant="outlined" fullWidth/>
-      <TextField id="sobrenome" label="Sobrenome"  variant="outlined" fullWidth/>
-      <TextField id="CPF" label="CPF"  variant="outlined" fullWidth/>
+      <TextField id="nome" label="Nome" variant="outlined"  margin="normal" fullWidth/>
+      <TextField id="sobrenome" label="Sobrenome"  variant="outlined" margin="normal" fullWidth/>
+      <TextField id="CPF" label="CPF"  variant="outlined" margin="normal" fullWidth/>
 
       <label>Promoções</label>
       <input type="checkbox" />
