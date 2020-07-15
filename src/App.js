2bc8a8171e5df4070c63973c8948a87bd6dc0e3b
Ricardo <dones.bugan@gmail.com>
2020-07-15 11:29:36 -0300

    video 5.2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <Container component="article" maxWidth="sm">
         <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
-        <FormularioCadastro aoEnviar={aoEnviarForm} />
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
       </Container>
     );
   }
@@ -17,6 +17,14 @@ class App extends Component {
 
 function aoEnviarForm(dados){
   console.log(dados);
+}
+
+function validarCPF(cpf){
+  if(cpf.length !== 11){
+    return {valido:false, texto:"CPF deve ter 11 digitos."}
+  }else{
+    return {valido:true, texto:""}
+  }
 }
 
 export default App;
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 
-function FormularioCadastro({aoEnviar}) {
+function FormularioCadastro({aoEnviar, validarCPF}) {
   const [nome, setNome] = useState("");
   const [sobrenome, setSobrenome] = useState("");
   const [cpf, setCpf] = useState("");
   const [promocoes, setPromocoes] = useState(true);
   const [novidades, setNovidades] = useState(false);
-
-  
+  const [erros, setErros] = useState({cpf:{valido:true, texto:""}})
   return (
     <form
       onSubmit={(event) => {
@@ -43,6 +42,13 @@ function FormularioCadastro({aoEnviar}) {
         onChange={(event) => {
           setCpf(event.target.value);
         }}
+
+        onBlur={(event)=>{
+          const ehValido = validarCPF(cpf);
+          setErros({cpf:ehValido})
+        }}
+        error={!erros.cpf.valido}
+        helperText={erros.cpf.texto}
         id="CPF"
         label="CPF"
         variant="outlined"
