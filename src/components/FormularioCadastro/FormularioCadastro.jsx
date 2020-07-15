559e8513714ac6684d5c8b508409f73d40243002
Ricardo <dones.bugan@gmail.com>
2020-07-15 10:20:38 -0300

    video 3.3

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,23 +1,26 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
-  const [nome,setNome] = useState("Ricardo");
-  
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        console.log(nome);
+        console.log(nome, sobrenome);
       }}
     >
       <TextField
         value={nome}
         onChange={(event) => {
-            setNome(event.target.value);
-          if (nome.length >= 3) {
-            setNome(nome.substr(0, 3));
+          let tmpNome = event.target.value;
+          if (tmpNome.length >= 3) {
+            tmpNome = tmpNome.substr(0, 3);
           }
+
+          setNome(tmpNome);
         }}
         id="nome"
         label="Nome"
@@ -26,6 +29,10 @@ function FormularioCadastro() {
         fullWidth
       />
       <TextField
+        value={sobrenome}
+        onChange={(event) => {
+          setSobrenome(event.target.value);
+        }}
         id="sobrenome"
         label="Sobrenome"
         variant="outlined"
